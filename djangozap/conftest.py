ef2ce377e882120308f46c45e214d3d03aa822bd
Fabio Martinelli <webmaster@zupy.com.br>
2025-02-17 11:38:43 -0300

    Inicialização do DjangoZap 🚀

---
+++ djangozap/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djangozap.users.models import User
+from djangozap.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
